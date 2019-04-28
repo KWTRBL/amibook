f51bce87b77145808fdb67d0a4ebc7c136125abd
Xiitsuh <kaew_prw98@hotmail.com>
2019-04-28 17:57:29 +0700

    reset code

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-        <div id="login" style="  height: 100%;background-position:center;background-image: url(https://scontent.fbkk2-6.fna.fbcdn.net/v/t1.15752-9/58790261_419524932210467_7517835296637976576_n.png?_nc_cat=111&_nc_ht=scontent.fbkk2-6.fna&oh=454c9c22111642f4554d85c2169add6a&oe=5D7240DB)  ; ">
+        <div id="login" style="  height: 100%;background-position:center;background-image: url(https://scontent.fbkk1-5.fna.fbcdn.net/v/t1.15752-9/59022624_393867608008046_8169876551249166336_n.png?_nc_cat=111&_nc_ht=scontent.fbkk1-5.fna&oh=f20bdde971991e87b61eaa4230a3f469&oe=5D390619)  ; ">
             <br>
             <!-- <a-button type="primary">Primary</a-button> -->
             <loginPanel />
